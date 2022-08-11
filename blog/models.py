28d60e371f00f395bd2e4175113c99c556cf1648
geum6333 <geum6333@gmail.com>
2022-08-11 16:15:39 +0900

    미리보기와 요약문 추가하기

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ import os
 
 class Post(models.Model):
     title = models.CharField(max_length=50)  # 게시글 제목
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()  # 게시글 본문 내용
     
     created_at = models.DateTimeField(auto_now_add=True)  # 게시글 게시 날짜
@@ -16,9 +17,9 @@ class Post(models.Model):
         return f'[{self.pk}]{self.title}'
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
-    def get_file_exit(self):
+    def get_file_name(self):
         return os.path.basename(self.file_upload.name)
-    def get_file_exit(self):
+    def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
 # Create your models here.
 
