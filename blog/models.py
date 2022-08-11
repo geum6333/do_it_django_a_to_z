266236434689a3570b2d950126e0e48719450062
geum6333 <geum6333@gmail.com>
2022-08-11 15:40:51 +0900

    첨부파일 있을 경우 포스트 상세 페이지에 다운로드 버튼 출력

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 
 class Post(models.Model):
     title = models.CharField(max_length=50)  # 게시글 제목
@@ -15,5 +16,9 @@ class Post(models.Model):
         return f'[{self.pk}]{self.title}'
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
+    def get_file_exit(self):
+        return os.path.basename(self.file_upload.name)
+    def get_file_exit(self):
+        return self.get_file_name().split('.')[-1]
 # Create your models here.
 
