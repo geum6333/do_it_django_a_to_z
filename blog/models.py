7d7a316fbb0c3f086e43ce1e6b5754f758fd2b2d
geum6333 <geum6333@gmail.com>
2022-08-11 13:44:51 +0900

    포스트 이미지 출력

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,8 @@ class Post(models.Model):
     
     created_at = models.DateTimeField(auto_now_add=True)  # 게시글 게시 날짜
     updated_at = models.DateTimeField(auto_now=True)
+
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     # auther: 추후 작성 예정
     def __str__(self):
         return f'[{self.pk}]{self.title}'
