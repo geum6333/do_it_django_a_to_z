2bc96e59a2b27638360935c669aaa1db9d6d64b6
geum6333 <geum6333@gmail.com>
2022-08-16 16:24:02 +0900

    포스트 목록 페이지 카테고리 기능 추가

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 
 
 class Post(models.Model):
@@ -15,7 +25,8 @@ class Post(models.Model):
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
-    author = models.ForeignKey(User, null=True ,on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
     # 포스트 제목
     def __str__(self):
