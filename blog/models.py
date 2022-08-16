06c63e734deb66689ee3c91f61a67aeccbe6fd32
geum6333 <geum6333@gmail.com>
2022-08-16 11:25:50 +0900

    포스트 상세페이지 작성자 정보 추가

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+
+
 class Post(models.Model):
     title = models.CharField(max_length=50)  # 게시글 제목
     hook_text = models.CharField(max_length=100, blank=True)
